a0b43cfa20902ba7e1475b7783b38a780266927c
Stephen Adesina <bauerstevenson@gmail.com>
2023-01-04 23:46:49 +0000

    test to see if tailwind is enabled

--- ui/pages/index.jsx
+++ ui/pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Link from 'next/link';
-import '../styles/globals.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
